6946f79549d635763b9c3ecfc229872c8e1b45fb
Korman Chen <kchen93@mail.ccsf.edu>
2018-02-25 23:26:28 -0800

    Buttons, labels, and more constraints on loginVC

--- FitKit/Controller/LoginViewController.swift
+++ FitKit/Controller/LoginViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Firebase
+import HexColors
 
 class LoginViewController: UIViewController {
 
     //MARK: Properties
     let logoImage: UIImageView = {
         let image = UIImageView()
+        image.backgroundColor = UIColor.red
         image.contentMode = .scaleAspectFill
         image.translatesAutoresizingMaskIntoConstraints = false
         image.layer.masksToBounds = true
@@ -22,14 +25,17 @@ class LoginViewController: UIViewController {
     
     let inputContainer: UIView = {
         let view = UIView()
-        view.layer.cornerRadius = 12
+        view.layer.cornerRadius = 8
         view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
         
         return view
     }()
     
     let emailTextField: UITextField = {
         let textfield = UITextField()
+        textfield.font = UIFont(name: (textfield.font?.fontName)!, size: 14)
         textfield.translatesAutoresizingMaskIntoConstraints = false
         textfield.layer.masksToBounds = true
         textfield.placeholder = "Email"
@@ -37,8 +43,18 @@ class LoginViewController: UIViewController {
         return textfield
     }()
     
+    let inputSeparator: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 250, g: 250, b: 250)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        
+        return view
+    }()
+    
     let passwordTextField: UITextField = {
         let textfield = UITextField()
+        textfield.font = UIFont(name: (textfield.font?.fontName)!, size: 14)
         textfield.translatesAutoresizingMaskIntoConstraints = false
         textfield.layer.masksToBounds = true
         textfield.isSecureTextEntry = true
@@ -47,23 +63,84 @@ class LoginViewController: UIViewController {
         return textfield
     }()
     
+    let loginButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 4
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.masksToBounds = true
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = UIColor("#302939")
+        
+        return button
+    }()
+    
+    let signupLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Don't have an account? Create one"
+        label.font = UIFont(name: (label.font?.fontName)!, size: 8)
+        label.textColor = UIColor("#FAFAFA")
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.masksToBounds = true
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViewConstraints()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        fatalError("Using too much memory.")
     }
     
     //MARK: Setup view constraints
     func setupViewConstraints() {
-        view.backgroundColor = UIColor.darkGray
+        view.backgroundColor = UIColor("#50595C")
         view.addSubview(logoImage)
         view.addSubview(inputContainer)
+        view.addSubview(loginButton)
+        view.addSubview(signupLabel)
+        inputContainer.addSubview(emailTextField)
+        inputContainer.addSubview(passwordTextField)
+        inputContainer.addSubview(inputSeparator)
+
+        inputContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainer.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputContainer.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
-        logoImage.widthAnchor.constraint(equalToConstant: 150)
-        logoImage.heightAnchor.constraint(equalToConstant: 150)
-        logoImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 50)
+        logoImage.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        logoImage.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        logoImage.bottomAnchor.constraint(equalTo: inputContainer.topAnchor, constant: -50).isActive = true
+        logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        emailTextField.leadingAnchor.constraint(equalTo: inputContainer.leadingAnchor, constant: 8).isActive = true
+        emailTextField.trailingAnchor.constraint(equalTo: inputContainer.trailingAnchor).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: inputContainer.topAnchor).isActive = true
+        emailTextField.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 0.5).isActive = true
+        
+        inputSeparator.leadingAnchor.constraint(equalTo: inputContainer.leadingAnchor).isActive = true
+        inputSeparator.trailingAnchor.constraint(equalTo: inputContainer.trailingAnchor).isActive = true
+        inputSeparator.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        inputSeparator.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        
+        passwordTextField.leadingAnchor.constraint(equalTo: inputContainer.leadingAnchor, constant: 8).isActive = true
+        passwordTextField.trailingAnchor.constraint(equalTo: inputContainer.trailingAnchor).isActive = true
+        passwordTextField.topAnchor.constraint(equalTo: inputSeparator.bottomAnchor).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 0.5).isActive = true
+        
+        loginButton.leadingAnchor.constraint(equalTo: inputContainer.leadingAnchor).isActive = true
+        loginButton.trailingAnchor.constraint(equalTo: inputContainer.trailingAnchor).isActive = true
+        loginButton.topAnchor.constraint(equalTo: inputContainer.bottomAnchor, constant: 75).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        signupLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        signupLabel.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 10).isActive = true
+        signupLabel.heightAnchor.constraint(equalToConstant: 10).isActive = true
     }
 }
 
@@ -75,3 +152,8 @@ extension LoginViewController: UITextFieldDelegate {
     }
 }
 
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
