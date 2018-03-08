58d45ff1839bdd9e2866a4b2b16b407ad4b261e3
Korman Chen <kchen93@mail.ccsf.edu>
2018-03-08 12:02:26 -0800

    separated some functions

--- FitKit/Controller/CalculatorViewController.swift
+++ FitKit/Controller/CalculatorViewController.swift
@@ -13,8 +13,10 @@ import Firebase
 class CalculatorViewController: UIViewController {
     
     private var goalButtons = [UIButton]()
+    private var textFields = [UITextField]()
+    private var goalSelected = false
     
-    let container: UIStackView = {
+    let buttonContainer: UIStackView = {
         let stackview = UIStackView()
         stackview.translatesAutoresizingMaskIntoConstraints = false
         stackview.distribution = .fillEqually
@@ -23,13 +25,61 @@ class CalculatorViewController: UIViewController {
         return stackview
     }()
     
+    let textFieldContainer: UIStackView = {
+        let stackview = UIStackView()
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.distribution = .fillEqually
+        stackview.axis = .vertical
+        
+        return stackview
+    }()
+    
+    let caloriesTextField: UITextField = {
+        let tf = UITextField()
+        tf.layer.masksToBounds = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Maintenance Calories"
+        
+        return tf
+    }()
+    
+    let carbTextField: UITextField = {
+        let tf = UITextField()
+        tf.layer.masksToBounds = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Carbohydrates"
+        tf.isHidden = true
+        
+        return tf
+    }()
+    
+    let proteinTextField: UITextField = {
+        let tf = UITextField()
+        tf.layer.masksToBounds = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Protein"
+        tf.isHidden = true
+        
+        return tf
+    }()
+    
+    let fatTextField: UITextField = {
+        let tf = UITextField()
+        tf.layer.masksToBounds = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Fat"
+        tf.isHidden = true
+        
+        return tf
+    }()
+    
     lazy var selectGoalButton: UIButton = {
         let button = UIButton()
         button.setTitle("Select Goal", for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.masksToBounds = true
         button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(handleSelect), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handleSelect(sender:)), for: .touchUpInside)
         
         return button
     }()
@@ -41,6 +91,7 @@ class CalculatorViewController: UIViewController {
         button.layer.masksToBounds = true
         button.backgroundColor = UIColor.blue
         button.isHidden = true
+        button.addTarget(self, action: #selector(handleSelect(sender:)), for: .touchUpInside)
         
         return button
     }()
@@ -52,6 +103,7 @@ class CalculatorViewController: UIViewController {
         button.layer.masksToBounds = true
         button.backgroundColor = UIColor.red
         button.isHidden = true
+        button.addTarget(self, action: #selector(handleSelect(sender:)), for: .touchUpInside)
         
         return button
     }()
@@ -63,6 +115,7 @@ class CalculatorViewController: UIViewController {
         button.layer.masksToBounds = true
         button.backgroundColor = UIColor.red
         button.isHidden = true
+        button.addTarget(self, action: #selector(handleSelect(sender:)), for: .touchUpInside)
         
         return button
     }()
@@ -87,30 +140,69 @@ class CalculatorViewController: UIViewController {
         goalButtons.append(maintainButton)
         goalButtons.append(gainWeightButton)
         
-        view.addSubview(container)
+        textFields.append(caloriesTextField)
+        textFields.append(carbTextField)
+        textFields.append(proteinTextField)
+        textFields.append(fatTextField)
+        
+        view.addSubview(buttonContainer)
+        view.addSubview(textFieldContainer)
         
         goalButtons.forEach { (button) in
-            container.addArrangedSubview(button)
-            button.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
-            button.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+            buttonContainer.addArrangedSubview(button)
+            button.leadingAnchor.constraint(equalTo: buttonContainer.leadingAnchor).isActive = true
+            button.trailingAnchor.constraint(equalTo: buttonContainer.trailingAnchor).isActive = true
             button.heightAnchor.constraint(equalToConstant: 60).isActive = true
         }
         
-        container.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        container.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        container.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        textFields.forEach { (textfield) in
+            textFieldContainer.addArrangedSubview(textfield)
+            textfield.leadingAnchor.constraint(equalTo: buttonContainer.leadingAnchor).isActive = true
+            textfield.trailingAnchor.constraint(equalTo: buttonContainer.trailingAnchor).isActive = true
+            textfield.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        }
+        
+        textFieldContainer.topAnchor.constraint(equalTo: buttonContainer.bottomAnchor).isActive = true
+        textFieldContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        textFieldContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        
+        buttonContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        buttonContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        buttonContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
     
-    @objc func handleSelect() {
-        goalButtons.forEach { (button) in
-            if button != selectGoalButton {
-                UIView.animate(withDuration: 0.44, animations: {
-                    button.isHidden = !button.isHidden
-                    self.view.layoutIfNeeded()
-                })
-            }
+    @objc func handleSelect(sender: UIButton) {
+        if sender != selectGoalButton {goalSelected = true}
+        animate(uiArray: goalButtons)
+        print(goalSelected)
+    }
+    
+    func animate(uiArray: [Any]) {
+        if let buttonArray = uiArray as? [UIButton] {
+            buttonArray.forEach({ (button) in
+                UIView.animate(withDuration: 0.44, delay: 0, options: .curveEaseIn, animations: {
+                    if button != self.selectGoalButton {
+                        button.isHidden = !button.isHidden
+                        self.view.layoutIfNeeded()
+                    }
+                }, completion: nil)
+            })
+        }
+        else if let tfArray = uiArray as? [UITextField] {
+            tfArray.forEach({ (tf) in
+                UIView.animate(withDuration: 0.44, delay: 0, options: .curveEaseIn, animations: {
+                    if tf != self.caloriesTextField {
+                        tf.isHidden = !tf.isHidden
+                        self.view.layoutIfNeeded()
+                    }
+                }, completion: nil)
+            })
         }
     }
     
+}
+
+extension CalculatorViewController: UITextFieldDelegate {
     
 }
